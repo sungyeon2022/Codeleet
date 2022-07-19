4999ba510117c54aa2dd5f9fb29e99c998ed8a76
josungyeon <zounsam@naver.com>
2022-07-19 10:41:02 +0900

    v0.3

--- C/C_leetCode/C_leetCode/test.c
+++ C/C_leetCode/C_leetCode/test.c
@@ -7,15 +7,11 @@
 int main() {
 	int num=3;
 	int m = num;
-	char rs[10000];
-	char* roman[] = { "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I" };
+	char rs[]="CD";
+	char roman[] = "MCMDCDCXCLXLXIXVIVI";
 	int in[] = { 1000,900,500,400,100,90,50,40,10,9,5,4,1 };
-	for (int i = 0; i < 13; i++) {
-		int nrs = m / in[i];
-		m %= in[i];
-		for (int j = 0; j < m; j++) {
-			strcat_s(rs,8,roman[i]);
-		}
-	}
-	printf("%c", rs);
+	char* a = roman;
+	char* b = rs;
+	*b = *a;
+	printf("%s", b);
 }
